b9304a3e95a3f48157c3754a1cbf26c4b15c3881
Cody Scott <austics@gmail.com>
2013-07-02 22:02:14 -0500

    Initial Commit

---
+++ URC-Shuttle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  URC-Shuttle
+//
+//  Created by Cody Scott on 7/2/13.
+//  Copyright (c) 2013 Cody Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
